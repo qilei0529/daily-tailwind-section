3e190da638baa0ef712489f3a02a47701d657c11
janbluus <qilei0529@gmail.com>
2025-08-01 15:13:42 +0700

    feat: add ui component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export const version = "2.6";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
