415db4b557bb293d564737869f9359799ae2fe01
janbluus <qilei0529@gmail.com>
2025-08-30 18:51:32 +0700

    feat: 2025-08-30

---
+++ src/app/daily/2025-08-27/page.tsx
@@ -0,0 +1,34 @@
+export default function Page() {
+  return <div className="w-screen">
+    <div className="h-[100px] hidden bg-slate-500/10 mb-8 text-center justify-center items-center flex text-2xl text-slate-300 font-bold">2025-08-27</div>
+    <div className="max-w-[1800px] relative mx-auto bg-gray-500/10 relative lg:bg-white-100 lg:h-screen">
+      <div className="grid lg:grid-cols-4 lg:h-screen">
+        <div className="col-span-2 bg-green-500/80">
+          <div className="h-full w-full bg-black/80 aspect-[16/9] lg:aspect-auto">3213123</div>
+        </div>
+        <div className="col-span-1 bg-green-500/30 aspect-[16/9] lg:aspect-auto">
+          2
+        </div>
+        <div className="col-span-1 bg-orange-500/30 aspect-[16/9] lg:aspect-auto">
+          3
+        </div>
+
+        <div className="col-span-1 bg-blue-500/30 aspect-[16/9] lg:aspect-auto">
+          2-1
+        </div>
+        <div className="col-span-1 bg-yellow-500/30 aspect-[16/9] lg:aspect-auto">
+          2-2
+        </div>
+        <div className="col-span-1 bg-purple-500/30 aspect-[16/9] lg:aspect-auto">
+          2-3
+        </div>
+        <div className="col-span-1 bg-pink-500/30 aspect-[16/9] lg:aspect-auto">
+          2-4
+        </div>
+      </div>
+    </div>
+    <div className="h-screen bg-white-500/10">
+      2
+    </div>
+  </div>
+}
